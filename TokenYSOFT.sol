4610abbaee7a947ec1b489f6bb54ac2f13420b02
yousoftco <yousoft.co@gmail.com>
2026-06-30 00:46:39 -0500

    change class name

--- TokenYSOFT.sol
+++ TokenYSOFT.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.31;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenYST is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+contract TokenYSOFT is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
     constructor(
         string memory name,
         string memory symbol,
